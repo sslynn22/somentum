d08c7486d3049ce94090f0c606d509777977b77c
sslynn22 <129486529+sslynn22@users.noreply.github.com>
2023-11-23 11:18:25 +0000

    아자

--- js/greeting.js
+++ js/greeting.js
@@ -1,7 +1,33 @@
+/**
+ * 2023.11.23 시나버스 shinahverse.kr
+ * 당신의 이름을 적어 주세요.
+ * 라고 사용자에게 물어서 답변을 화면에 표시한다.
+ */
+
+/**
+ * document에 있는 id가 frmLogin인 form.
+ * input box와 logIn 버튼으로 구성
+ */
 const frmLogin = document.querySelector("#frmLogin");
+
+/**
+ * document에 있는 input box.
+ */
 const inputLogin = document.querySelector("#frmLogin input");
+
+/**
+ * document에 있는 H1 요소.
+ */
 const greeting = document.getElementById("greeting");
+
+/**
+ * "hidden"문자열을 저장한 상수
+ */
 const HIDDEN_CLASSNAME = "hidden";
+
+/**
+ * "userName"문자열을 저장한 상수
+ */
 const KEY_USERNAME = "userName";
 
 /**
@@ -9,7 +35,7 @@ const KEY_USERNAME = "userName";
  */
 function onClickLoginBtn(event){
   //1) event의 기본 행동이 발생되지 않도록 막는다.
-  event.preventDefault();
+  event.preventDefault(); 
 
   //2) form을 다시 숨긴다.
   frmLogin.classList.add(HIDDEN_CLASSNAME);
@@ -22,30 +48,30 @@ function onClickLoginBtn(event){
 
   //5) 함수 호출
   displayGreeting(userName);
+}
 
-  /**
+/**
  * h1을 디스플레이하는 함수.
  * h1요소 안에 인사말 텍스트를 추가하고, 히든 클래스 제거
  */
-
 function displayGreeting(argName){
-    greeting.innerText = `hello ${argName}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-  }
-  
-  /**
-   * 로컬스토리지의 userName값을 담은 변수
-   */
-  //1.제일 처음 확인한다.
-  const lsUserName = localStorage.getItem("userName");
-  
-  if (lsUserName === null){
-      //form을 보여준다
-      frmLogin.classList.remove(HIDDEN_CLASSNAME);
-      //2.
-      frmLogin.addEventListener("submit", onClickLoginBtn);
-     
-  }else{ //로컬스토리지에 값이 저장되어 있다.
-      //h1을 보여준다.
-      displayGreeting(lsUserName);
-  }
+  greeting.innerText = `hello ${argName}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+/**
+ * 로컬스토리지의 userName값을 담은 변수
+ */
+//1.제일 처음 확인한다.
+const lsUserName = localStorage.getItem("userName");
+
+if (lsUserName === null){
+    //form을 보여준다
+    frmLogin.classList.remove(HIDDEN_CLASSNAME);
+    //2.
+    frmLogin.addEventListener("submit", onClickLoginBtn);
+    
+}else{ //로컬스토리지에 값이 저장되어 있다.
+    //h1을 보여준다.
+    displayGreeting(lsUserName);
+}
